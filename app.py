7e9388c0185837cd87682ab7d2fbe3fe85f7da35
Peezy0 <prince7okyere@gmail.com>
2025-04-18 15:52:09 -0400

    added embeddings

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_docs):
@@ -14,7 +17,21 @@ def get_pdf_text(pdf_docs):
     return text
 
 
-def get_chunks(raw_text):
+def get_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 
 def main():
@@ -36,6 +53,7 @@ def main():
                 text_chunks = get_chunks(raw_text)
 
                 # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 
 if __name__ == '__main__':
